f8bb52a8bfe5b40e65a903eac47de3452f1abe2d
Max Graw <max@graw-online.de>
2023-03-10 21:01:54 +0100

    Update mesh.py

--- app/mesh.py
+++ app/mesh.py
@@ -1,15 +1,6 @@
-"""
-Example of using the rf24_mesh module to operate the nRF24L01 transceiver as
-a Mesh network master node.
-"""
-from pyrf24 import RF24, RF24Network, RF24Mesh
+import time
 
-radio = RF24(22, 0)
-network = RF24Network(radio)
-mesh = RF24Mesh(radio, network)
-mesh.node_id = 0
-if not mesh.begin():
-    # if mesh.begin() returns false for a master node,
-    # then radio.begin() returned false.
-    raise OSError("Radio hardware not responding.")
-radio.print_pretty_details()
+def mesh_master():
+    while True:
+        time.sleep(1)
+        print("yes")
