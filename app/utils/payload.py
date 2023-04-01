acba28bd514364c93d94b318d5013fff4d823c4a
Max Graw <max@graw-online.de>
2023-04-01 17:54:26 +0200

    Update payload.py

--- app/utils/payload.py
+++ app/utils/payload.py
@@ -1,2 +1,22 @@
+import struct
+from constant import MESSAGETYPES_SLAVES
+
 def convert_payload(payload, type):
-    pass
+    if type not in MESSAGETYPES_SLAVES:
+        return None
+    
+    # Temp and Hum
+    if type == MESSAGETYPES_SLAVES[0]:
+        temp = None
+        hum = None
+
+        try:
+            temp = struct.unpack("f", payload[0:4])
+            hum = struct.unpack("f", payload[4:8])
+        except:
+            return None
+        
+        return {
+            "temp": temp[0],
+            "hum": hum[0]
+        }
