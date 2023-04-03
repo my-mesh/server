73dfff2e6c5ea27170b87979de1a22fe99df0340
Max Graw <max@graw-online.de>
2023-04-03 12:20:54 +0200

    update styling

--- app/static/js/navigation.js
+++
@@ -1,9 +0,0 @@
-const navigation = document.getElementById("navigation");
-const collapse = document.getElementById("collapse");
-
-collapse.addEventListener("click", () => {
-  navigation.classList.toggle("collapsed");
-  document.cookie = `collapsed=${
-    navigation.classList.contains("collapsed") ? "true" : "false"
-  }`;
-});
