d14ed3491793afe7d46208a8b6e93006144f0a6c
Max Graw <max@graw-online.de>
2023-04-11 15:55:58 +0200

    add some basic screen templates

--- app/routes/info/routes.py
+++ app/routes/info/routes.py
@@ -1,7 +1,22 @@
 from flask import render_template
+from app.db import get_db
 from app.routes.info import bp
+from app.utils.db import select
 
 
 @bp.get("/info")
 def index():
-    return render_template("/pages/info.html", page="info")
+    db = get_db()
+
+    screen = select(db, "screens", ["screens_id"], where="active = ?", args=(1,))[0]
+
+    print(screen["screens_id"])
+
+    if (screen["screens_id"] == 1):
+        return render_template("/pages/info/time.html", page="info")
+    
+    if (screen["screens_id"] == 2):
+        return render_template("/pages/info/date.html", page="info")
+    
+    if (screen["screens_id"] == 3):
+        return render_template("/pages/info/blank.html", page="info")
